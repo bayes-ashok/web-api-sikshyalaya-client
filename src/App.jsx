b89fb56b9df49c52836aef85101aebdd6e443ce8
Ashok Shah <bayes.ashok@gmail.com>
2025-01-04 20:57:36 +0545

    hero section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Navbar from "./components/navbar";
+import HeroSection from "./pages/student/HeroSection";
 function App() {
   return (
     <main>
+      <HeroSection></HeroSection>
       <Navbar></Navbar>
       <Login></Login>
     </main>
