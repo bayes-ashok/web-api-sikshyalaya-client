a9ab613aef4ecc26a0da890fecd2ddb32253dc6f
Ashok Shah <bayes.ashok@gmail.com>
2025-02-26 23:12:57 +0545

    added shadcn(fixed problem by reinstalling)

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
-import { Toaster } from "../src/components/ui/sonner";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/auth-context/index.jsx";
+import InstructorProvider from "./context/instructor-context/index.jsx";
+import StudentProvider from "./context/student-context/index.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <>
-      <App />
-      <Toaster></Toaster>
-  </>
+  <BrowserRouter>
+    <AuthProvider>
+      <InstructorProvider>
+        <StudentProvider>
+          <App />
+        </StudentProvider>
+      </InstructorProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );
